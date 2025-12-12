f84826244dda99e0b9b4c9965727a285f83106a8
Akshay <mandaakshay20@gmail.com>
2025-12-12 11:32:59 +0530

    Wrote code according to the given description

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
 import React from "react";
-import './../styles/App.css';
+import { Route, Switch } from "react-router-dom";
+import Navigation from "./Navigation";
+import Home from "./Home";
+import About from "./About";
 
-const App = () => {
+function App() {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Navigation />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
